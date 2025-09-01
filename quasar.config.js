be6afea853318e4a81588613a9e96ed0df7d9a5f
Julien Reichel <julien.reichel@bluewin.ch>
2025-09-01 12:31:04 +0200

    chore: fix to handle history mode SPA in github pages

--- quasar.config.js
+++ quasar.config.js
@@ -44,7 +44,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: process.env.NODE_ENV === 'production' ? '/bomb-buster/' : '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
